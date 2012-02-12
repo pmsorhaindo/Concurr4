f09f1b692cbf6dafa7f9e63f979a2bc2567d33d4
Mikey <pmsorhaindo@gmail.com>
2012-02-12 21:49:56 +0000

    ADA is killing me

---
+++ Answer/class_orderList.adb
@@ -0,0 +1,7 @@
+with Simple_io;
+use Simple_io;
+
+package body Class_orderList is
+
+
+
--- Answer/class_orderList.ads
+++ Answer/class_orderList.ads
@@ -1,9 +1,11 @@
 with Simple_io;
 use Simple_io;
 
-package Class_orderList {
-	
-        private static int nextOrderID = 0;
+package Class_orderList
+
+        protected type OrderList is
+        type theList is array (0 .. 9) of Order	
+        type nextOrderID is Integer range 0 .. Integer'Last; --:= 0;
         type Order is record
 	        orderID : Integer;
 	        timePlaced : Time; -- TODO
